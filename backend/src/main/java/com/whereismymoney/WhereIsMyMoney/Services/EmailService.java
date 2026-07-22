5a871a2c241da2ad6ba35e96a1193e4bf97c3a2b
therishabhrajora <rishabhrajora20gmail.com>
2026-07-22 18:19:35 +0530

    .

--- backend/src/main/java/com/whereismymoney/WhereIsMyMoney/Services/EmailService.java
+++ backend/src/main/java/com/whereismymoney/WhereIsMyMoney/Services/EmailService.java
@@ -2,7 +2,7 @@ package com.whereismymoney.WhereIsMyMoney.Services;
 
 import java.util.Date;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
